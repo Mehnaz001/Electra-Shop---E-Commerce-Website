11acd9350afd53985ec6ba51b517ce7fc7da543c
Mehnaz Sheikh <165507214+Mehnaz001@users.noreply.github.com>
2025-07-23 09:25:47 +0530

    Update FilterSection.jsx

--- src/components/FilterSection.jsx
+++ src/components/FilterSection.jsx
@@ -42,7 +42,7 @@ const FilterSection = ({search, setSearch, brand, setBrand, priceRange, setPrice
              {/* price range  */}
              <h1 className='mt-5 font-semibold text-xl mb-3'>Price Range</h1>
              <div className='flex flex-col gap-2'>
-                <label htmlFor="">Price Range: ${priceRange[0]} - ${priceRange[1]}</label>
+                <label htmlFor="">Price Range: ₹{priceRange[0]} - ₹{priceRange[1]}</label>
                 <input type="range" min="0" max="5000" name="" id="" value={priceRange[1]} onChange={(e)=>setPriceRange([priceRange[0], Number(e.target.value)])} className='transition-all'/>
              </div>
              <button className='bg-red-500 text-white rounded-md px-3 py-1 mt-5 cursor-pointer'
